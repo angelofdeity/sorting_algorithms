6ae5ace1feec361d5edeb3486b615ddfbee6c8f6
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-20 19:57:46 +0100

    Make Bubble Sort Betty compliant

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,17 +1,21 @@
 #include "sort.h"
 
 /**
-* bubble_sort - sorts an array of integers in ascending order using the Bubble sort algorithm
+* bubble_sort - sorts an array of integers in ascending
+* order using the Bubble sort algorithm
 * @array: Pointer to the array to be sorted
 * @size: Size of the array
 */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i = 0, j;
-    int temp, swapped;
+	size_t i = 0, j;
+	int temp, swapped;
 
-    while (i < size - 1)
-    {
+	if (size < 2)
+		return;
+
+	while (i < size - 1)
+	{
 		swapped = 0;
 		j = 0;
 		while (j < size - i - 1)
@@ -19,7 +23,7 @@ void bubble_sort(int *array, size_t size)
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
-				array[j] = array[j +1];
+				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				swapped = 1;
 			}
@@ -28,6 +32,6 @@ void bubble_sort(int *array, size_t size)
 		}
 		if (!swapped)
 			break;
-        i++;
-    }
+		i++;
+	}
 }
