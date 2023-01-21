8fb73115a8360b5bf9ac370ece57d35675fa546c
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-21 08:42:58 +0100

    Low battery precaution

---
+++ 100-shell_sort.c
@@ -0,0 +1,19 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Sorts an array of integers using
+ * shell sort algorithm (Knuth sequence)
+ * @array: The array to sort
+ * @size: The size of the array
+*/
+void shell_sort(int *array, size_t size)
+{
+	int gap = size / 2;
+	int i, j;
+
+	while (gap > 0)
+	{
+		for (i = gap; i )
+		gap /= 2;
+	}
+}
