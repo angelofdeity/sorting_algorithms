efd5a1d5467219a121ab50c4e6df78704cb15f53
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-23 16:13:14 +0100

    Merge sort in progress

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,6 +1,19 @@
 #include "sort.h"
 
 /**
+ * merge - Sorts an array of integers, by merging
+ * the left half and the right half
+ * @array: Input array
+ * @left_half: Left half of the array
+ * @right_half: Right half of the array
+ * @size: Size of the array
+*/
+void merge(int *array, int *left_half, int *right_half, size_t size)
+{
+
+}
+
+/**
  * merge_sort - Sorts an array of integers in ascending order
  * using merge sort
  * @array: Array to sort
@@ -8,13 +21,26 @@
 */
 void merge_sort(int *array, size_t size)
 {
-	int i, left_size, right_size;
-	int left[size / 2], right[size/ 2 + 1];
+	size_t middle = size / 2, i, j;
+	int left_half[middle], right_half[size - middle];
 
 	if (size < 2)
 		return;
 
-	left_size = size / 2;
-	right_size = size - left_size;
+	printf("Merging...\n");
+	for (i = 0; i < middle; i++)
+		left_half[i] = array[i];
+	printf("[left]:");
+	print_array(left_half, middle);
 
+	for (j = 0, i = middle; i < size; i++, j++)
+		right_half[j] = array[i];
+	printf("[right]:");
+	print_array(right_half, size - middle);
+	printf("[Done]\n");
+
+	merge_sort(left_half, (size_t)middle);
+	merge_sort(right_half, size - middle);
+
+	merge(array, left_half, right_half, size);
 }
--- sort.h
+++ sort.h
@@ -40,5 +40,6 @@ int *sort_count(int *array, size_t size, int *occurrence);
 void sum_occurrence(int *occurrence, int max);
 void occurrence_set(int *array, size_t size, int *occurrence);
 void merge_sort(int *array, size_t size);
+void merge(int *array, int *left_half, int *right_half, size_t size);
 
 #endif
