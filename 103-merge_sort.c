85337712cfc1f344c9b045e048970e974528e37f
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-23 18:31:06 +0100

    Merge

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -7,49 +7,59 @@
  * @left_half: Left half of the array
  * @right_half: Right half of the array
  * @size: Size of the array
-*/
-/**
-void merge(int *array, int *left_half, int *right_half, size_t size)
+ */
+
+void split(int *array, size_t size, int *new)
 {
-	;
+	size_t i, middle = size / 2;
+
+	if (size < 2)
+		return;
+
+	printf("Merging...\n");
+	for (i = 0; i < middle; i++)
+		new[i] = array[i];
+	printf("[left]:");
+	print_array(new, middle);
+
+	for (i = middle; i < size; i++)
+		new[i] = array[i];
+	printf("[right]:");
+	print_array(new + middle, size - middle);
+	printf("[Done]\n");
+
+	split(new, middle, new);
+	split(new, size - middle, new);
 }
-*/
+
+void merge(int *array, int *new, size_t size)
+{
+	int i, j, middle = size / 2, k;
+
+	for (i = 0, j = middle, k = 0; i < middle && j < size; i++, j++)
+	{
+		if (new[i] < new[j])
+			array[k] = 
+	}
+}
 
 /**
  * merge_sort - Sorts an array of integers in ascending order
  * using merge sort
  * @array: Array to sort
  * @size: Size of the array
-*/
+ */
 void merge_sort(int *array, size_t size)
 {
-	size_t middle = size / 2, i, j;
-	int *left_half, *right_half;
-
-	left_half = malloc(sizeof(int) * middle);
-	right_half = malloc(sizeof(int) * size - middle);
+	int *new;
 
 	if (size < 2)
-	{
-		free(left_half);
-		free(right_half);
 		return;
-	}
 
-	printf("Merging...\n");
-	for (i = 0; i < middle; i++)
-		left_half[i] = array[i];
-	printf("[left]:");
-	print_array(left_half, middle);
+	new = malloc(sizeof(int) * size);
 
-	for (j = 0, i = middle; i < size; i++, j++)
-		right_half[j] = array[i];
-	printf("[right]:");
-	print_array(right_half, size - middle);
-	printf("[Done]\n");
-
-	merge_sort(left_half, (size_t)middle);
-	merge_sort(right_half, size - middle);
+	split(array, size, new);
 
 	/*merge(array, left_half, right_half, size);*/
+	free(new);
 }
