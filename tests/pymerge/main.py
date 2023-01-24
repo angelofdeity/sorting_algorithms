4d4d489f6a2a664b076352621582578cb080dcb9
Nico <lawani.gk@gmail.com>
2023-01-24 14:19:02 +0100

    add python merge_sort function

---
+++ tests/pymerge/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from merge import merge_sort
+array = [8, 7, 6, 1, 0, 9, 2, 13]
+print(array)
+merge_sort(array)
+print(array)
--- tests/pymerge/merge.py
+++ tests/pymerge/merge.py
@@ -1,21 +1,29 @@
 def merge_sort(array):
-    middle = len(array) / 2
-    left = array[:middle]
-    right = array[middle:]
-    merge_sort(left)
-    merge_sort(right)
+    if len(array) > 1:
+        middle = len(array) // 2
+        left = array[:middle]
+        right = array[middle:]
+        merge_sort(left)
+        merge_sort(right)
+        print(f"Merging...\n[left]: ", left)
+        print("[right]: ", right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                array[k] = left[i]
+                i += 1
+            else:
+                array[k] = right[j]
+                j += 1
+            k += 1
 
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
+        while i < len(left):
             array[k] = left[i]
             i += 1
-        else:
+            k += 1
+
+        while j < len(right):
             array[k] = right[j]
             j += 1
-        k += 1
-
-    while i < len(left):
-        array[k] = left[i]
-
-    while j < len(right):
-        array[k] = right[j]
+            k += 1
+        print("[Done]: ", array)
